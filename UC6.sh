a60445770addbecbc7a9d41a3693ec0e50816c98
“krbasu” <kaustavbasu4314@gmail.com>
2021-11-07 12:36:08 -0800

    ADDED UC6.sh

---
+++ UC6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter three number : " c
+
+x=$(( $a+$b*$c ))
+y=$(( $a*$b+$c ))
+z=`echo "scale=2; $c + $a / $b" | bc`
+w=$(( $a%$b+$c ))
+
+declare -A arithmeticDict
+
+arithmeticDict[1]=$x
+arithmeticDict[2]=$y
+arithmeticDict[3]=$z
+arithmeticDict[4]=$w
