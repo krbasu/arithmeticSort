fee481c037c98c082757454abe031f5cdfd286ce
“krbasu” <kaustavbasu4314@gmail.com>
2021-11-07 12:31:42 -0800

    ADDED UC4.sh

---
+++ UC4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter three number : " c
+
+x=$(( $a+$b*$c ))
+y=$(( $a*$b+$c ))
+z=`echo "scale=2; $c + $a / $b" | bc`
